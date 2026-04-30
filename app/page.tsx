9619056a7e47eb9a626dca40450f57f6e5a7dd6b
abdullah <abdullahhashmat964@gmail.com>
2026-04-30 16:05:15 +0500

    change in section file

--- app/page.tsx
+++ app/page.tsx
@@ -1,13 +1,15 @@
 import React from 'react'
 import Navbar from './components/Navbar'
 import Header from './components/Header'
+import Section from './components/section'
 
 const Home = () => {
   return (
     <>
    <Navbar/>
    <Header/>
-    </>
+   <Section/>
+       </>
   )
 }
 
