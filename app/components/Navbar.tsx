fe74fde4ec5ffbeeeb2f3ecdcc0c29807451e2c7
Dev-A <ahmadali522g@gmail.com>
2026-04-30 16:00:21 +0500

    Update Navbar.tsx
    changes in nav

--- app/components/Navbar.tsx
+++ app/components/Navbar.tsx
@@ -1,9 +1,18 @@
 import React from 'react'
 
-const Navbar = () => {
+const Header = () => {
   return (
-    <div>Navbar</div>
+    <div className="flex items-center justify-between py-5">
+      <nav className='flex items-center'>
+        <ul>
+          <li>Home</li>
+          <li>About</li>
+          <li>Services</li>
+          <li>Contact</li>
+        </ul>
+      </nav>
+    </div>
   )
 }
 
-export default Navbar
+export default Header
